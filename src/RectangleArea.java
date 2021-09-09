9587dfffc82bc3af43c95500092c8fb0d2fe6abd
EliFJ <jonesel@ssfs.org>
2021-09-09 10:59:42 -0600

    Wrote Area= 84 and printed equations

--- src/RectangleArea.java
+++ src/RectangleArea.java
@@ -3,6 +3,12 @@ public class RectangleArea {
 
     public static void main(String[] args) {
         int length = 14;
+        int width = 6;
+        int area = length*width;
+        System.out.print(("Area = ")+width*length);
+
+
+
         //declare and initialize width
 
         //declare area - use length and width to calculate area
