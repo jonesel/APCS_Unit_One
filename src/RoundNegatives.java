15ee6364dba3af6659f4369ed87d84e859e00891
EliFJ <jonesel@ssfs.org>
2021-09-16 09:01:42 -0600

    rounded decimal

--- src/RoundNegatives.java
+++ src/RoundNegatives.java
@@ -4,6 +4,14 @@ public class RoundNegatives {
 
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a negative decimal number:");
+        double decimal = scan.nextDouble();
+        double roundedNum = (int)(decimal - .5);
+        System.out.print(decimal + " rounded to a whole number is " + roundedNum);
+
+
+
         // Create a Scanner object and write a prompt for the user to enter a number
 
         // Declare a variable of the correct type that will receive the input from the user.
--- src/RoundTenth.java
+++ src/RoundTenth.java
@@ -4,6 +4,14 @@ public class RoundTenth {
 
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter a positive decimal number:");
+        double decimal = scan.nextDouble();
+        double multiplied = decimal*10;
+        double roundedNum = (int)(multiplied+.5);
+        double answer = (double)(roundedNum/10);
+        System.out.print(decimal + " rounded to the nearest tenth is " + roundedNum);
+
         // Create a Scanner object
 
         // Prompt the user for a positive decimal number
